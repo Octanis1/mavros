06104e06703d493243157d5eb4f4dc0ccac3c31f
Sam Sulaimanov <sam@bitmorse.com>
2016-08-10 19:16:41 +0000

    modification of the covariance of the GPS

--- mavros/src/plugins/global_position.cpp
+++ mavros/src/plugins/global_position.cpp
@@ -133,14 +133,14 @@ private:
 		float epv = (raw_gps.epv != UINT16_MAX) ? raw_gps.epv / 1E2F : NAN;
 
 		if (!std::isnan(eph)) {
-			const double hdop = eph;
+			//const double hdop = eph;
+                        const double hdop = 0.1;
 
 			// From nmea_navsat_driver
-			fix->position_covariance[0 + 0] = \
-				fix->position_covariance[3 + 1] = std::pow(hdop, 2);
+			fix->position_covariance[0 + 0] = std::pow(hdop, 2);
+			fix->position_covariance[3 + 1] = std::pow(hdop, 2);
 			fix->position_covariance[6 + 2] = std::pow(2 * hdop, 2);
-			fix->position_covariance_type =
-					sensor_msgs::NavSatFix::COVARIANCE_TYPE_APPROXIMATED;
+			fix->position_covariance_type = sensor_msgs::NavSatFix::COVARIANCE_TYPE_APPROXIMATED;
 		}
 		else {
 			fill_unknown_cov(fix);
